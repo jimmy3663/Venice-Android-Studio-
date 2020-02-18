f9c378ba816b9737db629eb2b30dc5508c980182
jimmy3663 <jimmy3663@naver.com>
2020-02-18 12:40:16 +0900

    pop up activity for Graph

---
+++ app/src/main/java/com/example/sleep/pop4apnea.java
@@ -0,0 +1,14 @@
+package com.example.sleep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pop4apnea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop4apnea);
+    }
+}
